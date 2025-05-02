bed375abde109d4a0ad05d8a5dffac6db46bc04f
Kokai Laszlo <kokai.laszlo@gmail.com>
2025-05-02 14:45:21 -0400

    Task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -53,8 +53,21 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  // console.log(req.session);
+  if (isbn) {
+    const book = books[isbn];
+    if (book) {
+        const reviewer_name = req.session.authorization.username;
+        const review_text = req.body.review_text;
+        books[isbn].reviews[reviewer_name] = review_text;
+        
+        return res.status(200).json({message: "Review added."});  
+    } else {
+        return res.status(404).json({message: "Book not found."});  
+    }
+  }
+  return res.status(400).json({message: "Parameter error. ISBN is mandatory"});
 });
 
 module.exports.authenticated = regd_users;
