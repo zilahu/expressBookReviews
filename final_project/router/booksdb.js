c7f1564d4e743127a33f9ce46f61552ef32e3fe6
Kokai Laszlo <kokai.laszlo@gmail.com>
2025-05-02 14:03:13 -0400

    Task 5

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,13 +1,13 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"reviewer_name": "Otto Example", "review_text": "Superb!", "review_score": "10/10"} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {"reviewer_name": "Danika Example", "review_text": "Excellent!", "review_score": "9/10"} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {"reviewer_name": "Marika Example", "review_text": "Dark!", "review_score": "6/10"} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {"reviewer_name": "Otto Example", "review_text": "Superb!", "review_score": "10/10"} },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {"reviewer_name": "Otto Example", "review_text": "Superb!", "review_score": "10/10"} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {"reviewer_name": "Otto Example", "review_text": "Superb!", "review_score": "10/10"} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {"reviewer_name": "Otto Example", "review_text": "Superb!", "review_score": "10/10"} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {"reviewer_name": "Otto Example", "review_text": "Superb!", "review_score": "10/10"} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {"reviewer_name": "Otto Example", "review_text": "Superb!", "review_score": "10/10"} },
       10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -96,8 +96,16 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    if (isbn) {
+        const book = books[isbn];
+        if (book) {
+            res.send(JSON.stringify(book.reviews, null, 4));
+        } else {
+            return res.status(404).json({message: "Book not found."});  
+        }
+    }
+    return res.status(400).json({message: "Parameter error. ISBN is mandatory"});
 });
 
 module.exports.general = public_users;
