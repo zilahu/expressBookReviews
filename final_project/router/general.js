836d0a45de497773cc7421d385a545560748f926
Kokai Laszlo <kokai.laszlo@gmail.com>
2025-05-02 13:37:36 -0400

    task2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,7 +44,17 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
+    const isbn = req.params.isbn;
+    if (isbn) {
+        const book = books[isbn];
+        if (book) {
+            res.send(JSON.stringify(book, null, 4));
+        } else {
+            return res.status(404).json({message: "Book not found."});  
+        }
+    }
+
+    return res.status(400).json({message: "Parameter error. ISBN is mandatory"});
  });
   
 // Get book details based on author
