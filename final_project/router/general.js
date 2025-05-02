4491c7748b90ac89e3b6019dc3de18d464d105ba
Kokai Laszlo <kokai.laszlo@gmail.com>
2025-05-02 13:22:26 -0400

    task1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,14 +45,18 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books, null, 4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const book = books[author];
+  if (book) {
+    res.send(books[author]);  
+  } else {
+    return res.status(404).json({message: "Author not found!"});
+  }
 });
 
 // Get all books based on title
